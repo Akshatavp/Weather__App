deb6c53948cb5f96b163c58b35b38a38c874998c
Akshatavp <Patilakshata.2518@gmail.com>
2023-11-04 21:14:18 +0530

    Update

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const cardbg = document.querySelector(".card");
-
+const loader = document.querySelector(".loader");
 
 async function checkWeather(city) {
+
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (document.readyState !== "complete") {
+
+        document.querySelector(".loader").style.display = "block"
+
+    }
+
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block"
         document.querySelector(".weather").style.display = "none";
+        document.querySelector(".loader").style.display = "none"
+
 
     }
+
     else {
+
         var data = await response.json();
 
         document.querySelector(".city").innerHTML = data.name;
@@ -36,21 +48,29 @@ async function checkWeather(city) {
         else if (data.weather[0].main == "Mist") {
             cardbg.src = "imgs/misty.png";
         }
+
+
+
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
+        document.querySelector(".loader").style.display = "none"
+
 
 
     }
 
-
 }
 
 
+
 searchBtn.addEventListener("click", () => {
+
 
 
     checkWeather(searchBox.value);
 
 })
 
-checkWeather();
+checkWeather()
+
+checkready()
